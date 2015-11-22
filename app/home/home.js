84a39abd532a18f8e664effb4c88ba3fc621fda7
Brian Hines <brian@projectweekend.net>
2015-11-21 22:45:12 -0600

    This works for now

--- app/home/home.js
+++ app/home/home.js
@@ -11,34 +11,51 @@ angular.module('myApp.home', ['ngRoute'])
 
 .controller('HomeCtrl', ['$scope', '$websocket', function($scope, $websocket) {
 
+    var socket;
+
     $scope.settings = {
         url: '',
         apiKey: ''
     };
     $scope.socketConnected = false;
-    $scope.picloudInfo = {};
+    $scope.picloudInfo = [];
+    $scope.infoCreatedAt = null;
+    $scope.isConnecting = false;
     $scope.errorMessage = "";
 
-    $scope.startSocket = function () {
+    $scope.connect = function () {
         $scope.errorMessage = "";
+        $scope.isConnecting = true;
         var websocketUrl = $scope.settings.url + "?apiKey=" + $scope.settings.apiKey;
-        var socket = $websocket(websocketUrl);
+        socket = $websocket(websocketUrl);
         socket.onMessage(function (message) {
-            $scope.picloudInfo = JSON.parse(message.data);
+            var data = JSON.parse(message.data);
+            $scope.picloudInfo = formatInfoData(data);
+            $scope.infoCreatedAt = data.created_at;
         });
         socket.onOpen(function () {
+            $scope.isConnecting = false;
             $scope.socketConnected = true;
         });
-        socket.onError(function() {
+        socket.onError(function () {
+            $scope.isConnecting = false;
             $scope.socketConnected = false;
             $scope.resetSettings();
             $scope.errorMessage = "Socket error";
         });
-        socket.onClose(function() {
+        socket.onClose(function () {
+            $scope.isConnecting = false;
             $scope.socketConnected = false;
             $scope.resetSettings();
             $scope.errorMessage = "Socket closed";
         });
+    };
+
+    $scope.disconnect = function () {
+        socket.close();
+        $scope.isConnecting = false;
+        $scope.socketConnected = false;
+        $scope.resetSettings();
     };
 
     $scope.resetSettings = function () {
@@ -47,5 +64,24 @@ angular.module('myApp.home', ['ngRoute'])
             apiKey: ''
         };
     };
+
+    function formatInfoData (infoData) {
+        var result = [];
+        if (!infoData.events) {
+            return result;
+        }
+        infoData.events.forEach(function (eventItem) {
+            if (eventItem.connections) {
+                eventItem.connections.forEach(function (connectionItem) {
+                    result.push({
+                        name: eventItem.name,
+                        ipAddress: connectionItem.ip_address,
+                        connectedAt: connectionItem.connected_at
+                    });
+                });
+            }
+        });
+        return result;
+    }
 
 }]);
